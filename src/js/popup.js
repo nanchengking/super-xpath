d6cf4b20e51d67a157793dd5afe3a9d0c2a2c2de
刘述清 <liushuqing@weidian.com>
2016-07-20 17:30:06 +0800

    get trouble

--- src/js/popup.js
+++ src/js/popup.js
@@ -1,3 +1,10 @@
 /**
  * Created by liushuqing on 16/7/20.
  */
+
+$(document).ready(function () {
+    $('#start').click(function () {
+        console.log("点击了按钮");
+        alert("你好啊啊 啊啊啊啊");
+    });
+})
